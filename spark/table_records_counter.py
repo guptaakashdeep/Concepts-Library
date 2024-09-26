5eece7b8ce3072d07c9ca050cdd1829d5b7fe80b
Akashdeep Gupta <akashdeepgupta@Akashdeeps-MacBook-Air.local>
2024-09-26 20:22:35 +0530

    Added in code documentation and reformatting.

--- spark/table_records_counter.py
+++ spark/table_records_counter.py
@@ -1,36 +1,54 @@
+"""Example of Horizontal Parallelism in Spark"""
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit
 import argparse
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
-import json
 
 
 # Creating SparkSession
 spark = SparkSession.builder.master("yarn").enableHiveSupport().getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
-spark.conf.set("hive.exec.dynamic.partition.mode","nonstrict")
-spark.conf.set("spark.sql.sources.partitionOverwriteMode","dynamic")
-spark.conf.set("spark.sql.sources.commitProtocolClass", 
-                "org.apache.spark.sql.execution.datasources.SQLEmrOptimizedCommitProtocol")
+spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
+spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
+spark.conf.set(
+    "spark.sql.sources.commitProtocolClass",
+    "org.apache.spark.sql.execution.datasources.SQLEmrOptimizedCommitProtocol",
+)
 
 count_list = []
 
-def get_count(table_name: str) -> int:
+
+def get_count(table_name: str) -> dict:
+    """Gets the number of records count in a table.
+
+    Args:
+        table_name (str): Name of the table.
+
+    Returns:
+        dict: Dictionary with keys "table_name" and "count"
+    """
     try:
         count_dict = {}
         table_count = spark.sql(f"select count(*) from {table_name}").collect()[0][0]
         count_dict["table_name"] = table_name
         count_dict["count"] = table_count
         return count_dict
-    except Exception as e:
+    except Exception:
         count_dict["table_name"] = table_name
-        count_dict[f"count"] = 0
+        count_dict["count"] = 0
         return count_dict
 
 
 def main(tables: list) -> None:
+    """Main method to submit count jobs in parallel.
 
+    Args:
+        tables (list): list of table name.
+
+    Raises:
+        e: Exception in case of any failures
+    """
     with ThreadPoolExecutor(max_workers=6) as executor:
         to_do_map = {}
         for table in tables:
@@ -57,7 +75,7 @@ if __name__ == "__main__":
         "--tables",
         nargs="*",
         required=True,
-        help="List of tables for which count needs to be checked."
+        help="List of tables for which count needs to be checked.",
     )
 
     args = parser.parse_args()
@@ -70,8 +88,9 @@ if __name__ == "__main__":
     # Create dataframe
     table_count_df = spark.createDataFrame(count_list)
     table_count_df = table_count_df.withColumn("insert_ts", lit(datetime.now()))
-    
-    # Write dataframe into a control table.
-    col_order = spark.read.table(SYNC_CNT_TABLE).limit(1).columns
-    table_count_df.select(*col_order).coalesce(1).write.insertInto(CNT_TABLE, overwrite=True)
 
+    # Write dataframe into a control table.
+    col_order = spark.read.table(CNT_TABLE).limit(1).columns
+    table_count_df.select(*col_order).coalesce(1).write.insertInto(
+        CNT_TABLE, overwrite=True
+    )
